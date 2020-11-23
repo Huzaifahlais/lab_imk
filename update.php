b3d9ffa55d1593229573a3dbc1c6c72043d81d75
Arafah Nur Ihza 191402012 <60013691+aranurihza@users.noreply.github.com>
2020-11-23 18:20:07 +0700

    Update update.php

--- update.php
+++ update.php
@@ -8,6 +8,25 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+	<script>
+function tampilkan(){
+  var nama_fak=document.getElementById("insert_form").fakultas.value;
+  if (nama_fak=="Fakultas Kedokteran")
+    {
+        document.getElementById("prodi").innerHTML="<option value='Program Studi Belum Terpilih'>Pilih Program Studi</option><option value='S1-Pendidikan Dokter'>S1-Pendidikan Dokter</option>";
+    }
+  else if (nama_fak=="Fakultas Ilmu Budaya")
+    {
+        document.getElementById("prodi").innerHTML=
+        " <option value='Program Studi Belum Terpilih'>Pilih Program Studi</option><option value='S1-Satra Indonesia'>S1-Satra Indonesia</option><option value='S1-Satra Melayu'>S1-Satra Melayu</option><option value='S1-Satra Batak'>S1-Satra Batak</option><option value='S1-Satra Arab'>S1-Satra Arab</option><option value='inggris'>S1-Satra Inggris</option><option value='jepang'>S1-Satra Jepang</option><option value='S1-Sastra Cina'>S1-Sastra Cina</option><option value='S1-Ilmu Perpustakaan'>S1-Ilmu Perpustakaan</option><option value='S1-Ilmu Sejarah'>S1-Ilmu Sejarah</option><option value='S1-Etnomusikologi'>S1-Etnomusikologi</option>";
+    }
+
+      else if (nama_fak=="Fakultas Ilmu Komputer dan Teknologi Informasi")
+    {
+        document.getElementById("prodi").innerHTML="<option value='S1-Ilmu Komputer'>S1-Ilmu Komputer</option><option value='S1-Teknologi Informasi'>S1-Teknologi Informasi</option>";
+    }
+}
+</script>
     
   <style>
   	#corner{
@@ -43,6 +62,7 @@
 	$data = mysqli_query($koneksi,"SELECT *FROM perpus WHERE nim = '$nim'");
 	WHILE($d = mysqli_fetch_array($data)){
 	 ?>
+		 <form method="post" id="insert_form" name="insert_form" action="update_aksi.php">
 	 <div class="form-group">
 	 	<label class="col-sm-4 control-label">NIM <small id="small"> *</small></label>
 	 	<input class="form-control" type="text" name="nim" value="<?php echo $d['nim']; ?>" minlength="9" maxlength="9"required>
