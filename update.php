b2c0a7376d6e9d449b46aad590015eecb5026ecd
Haiqal Rizky R. 191402054 <56826724+haiqalrizkyr@users.noreply.github.com>
2020-11-23 20:28:42 +0700

    Update update.php
    penambahan script logo

--- update.php
+++ update.php
@@ -8,6 +8,7 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="https://kit.fontawesome.com/752056054c.js" crossorigin="anonymous"></script>
 	<script>
 function tampilkan(){
   var nama_fak=document.getElementById("insert_form").fakultas.value;
