a7dd7dde32f3b46651a05df1d1695c00171b52ce
Haiqal Rizky R. 191402054 <56826724+haiqalrizkyr@users.noreply.github.com>
2020-11-23 19:40:40 +0700

    Add files via upload
    Penambahan Footer

---
+++ footer.php
@@ -0,0 +1,80 @@
+<br>
+<footer class="page-footer font-small">
+
+  <div style="background-color: lightgrey;">
+    <div class="container">
+
+      <div class="row py-4 d-flex align-items-center">
+        <div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
+          <h6 class="mb-0">Get connected with us on social networks!</h6>
+        </div>
+
+        <div class="col-md-6 col-lg-7 text-center text-md-right">
+
+          <a class="fb-ic">
+            <i class="fab fa-facebook-f white-text mr-4"> </i>
+          </a>
+
+          <a class="tw-ic">
+            <i class="fab fa-twitter white-text mr-4"> </i>
+          </a>
+
+          <a class="gplus-ic">
+            <i class="fab fa-google-plus-g white-text mr-4"> </i>
+          </a>
+
+          <a class="li-ic">
+            <i class="fab fa-linkedin-in white-text mr-4"> </i>
+          </a>
+
+          <a class="ins-ic">
+            <i class="fab fa-instagram white-text"> </i>
+          </a>
+
+        </div>
+
+      </div>
+
+    </div>
+  </div>
+
+  <div class="container text-center text-md-left mt-5">
+
+    <div class="row mt-3">
+
+      <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+
+        <h6 class="text-uppercase font-weight-bold">Perpustakaan Universitas Sumatera Utara</h6>
+        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <img src="./usu.png" width="120px" align="left" />
+
+      </div>
+
+      <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+
+        <h6 class="text-uppercase font-weight-bold">Useful links</h6>
+        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>
+          <a href="#!">Your Account</a>
+        </p>
+        <p>
+          <a href="#!">Help</a>
+        </p>
+
+      </div>
+      <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+        <h6 class="text-uppercase font-weight-bold">Contact</h6>
+        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>
+          <i class="fas fa-home mr-3"></i> Jalan Dr. T. Mansur No. 9, Kampus Padang Bulan, Medan, 20155, Sumatera Utara</p>
+        <p>
+          <i class="fas fa-envelope mr-3"></i> info@usu.ac.id</p>
+        <p>
+          <i class="fas fa-phone mr-3"></i> (061) 8211633</p>
+      </div>
+    </div>
+  </div>
+  <div class="footer-copyright text-center py-3">© 2020 Copyright.Universitas Sumatera Utara
+  </div>
+
+</footer>
